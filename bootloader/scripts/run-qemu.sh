155970aade6159b114aa57f608333bbc33e66bed
PSOrigins10 <parav.sharma.328@gmail.com>
2026-09-19 13:57:20 +0530

    bootloader: fix qemu disk image path

--- bootloader/scripts/run-qemu.sh
+++ bootloader/scripts/run-qemu.sh
@@ -2,5 +2,6 @@
 set -e
 
 qemu-system-x86_64 \
+  -serial stdio \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
-  -drive format=raw,file=build/disk.img
+  -drive format=raw,file=bootloader/build/disk.img
